5c5d99757eadd368747d0b49b06ced1107f1558e
hans.roelants@infront.be <hans.roelants@infront.be>
2018-03-09 12:01:09 +0100

    Initial commit

---
+++ Crm.Xrm.Plugins/SimplePlugin.cs
@@ -0,0 +1,19 @@
+using System;
+using Microsoft.Xrm.Sdk;
+
+namespace Crm.Xrm.Plugins
+{
+    public class SimplePlugin : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context =
+                (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory serviceFactory =
+                (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService organizationService =
+                (IOrganizationService)serviceProvider.GetService(typeof(IOrganizationService));
+
+        }
+    }
+}
